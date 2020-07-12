92e89d6760cb9a92b8ab7dcf7eda007431ddd4ff
Yokomon <macaulaymich@gmail.com>
2020-07-12 18:02:08 +0100

    new features

--- battleship.js
+++ battleship.js
@@ -27,9 +27,9 @@ var model = {
   shipLength: 3,
   shipsSunk: 0,
   ships: [
-    { locations: ["06", "16", "26"], hits: ["", "", ""] },
-    { locations: ["24", "34", "44"], hits: ["", "", ""] },
-    { locations: ["10", "11", "12"], hits: ["", "", ""] }
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] }
   ],
   fire: function(guess) {
     for (var i = 0; i < this.numShips; i++) {
@@ -60,6 +60,46 @@ var model = {
         return true;
       }
     }
+  },
+  generateShipLocations: function() {
+    var locations;
+    for (var i = 0; i < this.numShips; i++) {
+      do {
+        locations = this.generateShip();
+      } while (this.collision(locations));
+      this.ships[i].locations = locations;
+    }
+  },
+  generateShip: function() {
+    var direction = Math.floor(Math.random() * 2);
+    var row, col;
+    if (direction === 1) {
+      row = Math.floor(Math.random() * this.boardSize);
+      col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+    } else {
+      row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+      col = Math.floor(Math.random() * this.boardSize);
+    }
+    var newShipLocations = [];
+    for (var i = 0; i < this.shipLength; i++) {
+      if (direction === 1) {
+        newShipLocations.push(row + "" + (col + i));
+      } else {
+        newShipLocations.push(row + i + "" + col);
+      }
+    }
+    return newShipLocations;
+  },
+  collision: function(locations) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = model.ships[i];
+      for (var j = 0; j < locations.length; j++) {
+        if (ship.locations.indexOf(locations[j]) >= 0) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 };
 
@@ -89,7 +129,7 @@ var controller = {
     }
   },
   parseGuess: function(guess) {
-    var alphabet = ["A", "B", "C", "D", "E", "F"];
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
     if (guess === null || guess.length != 2) {
       alert("This Isn't On The Board !!!");
     } else {
@@ -135,6 +175,8 @@ function init() {
 
   var guessInput = document.getElementById("guessInput");
   guessInput.onkeypress = handleKeyPress;
+
+  model.generateShipLocations();
 }
 
 function handleFireButton() {
